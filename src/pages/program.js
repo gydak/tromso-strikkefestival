f5d861fb20540bd167d5d324c6102989643b300e
Kristiane Westgård <kristianeaw@gmail.com>
2018-07-01 15:05:17 +0200

    Styling done

--- src/pages/program.js
+++ src/pages/program.js
@@ -10,24 +10,28 @@ export default function Program({ data }) {
   return (
     <div className="blog-posts">
       <h1>Program</h1>
-      <p>I år har vi et rikholdig program med alt fra foredrag innen Norsk ull, lokal produksjon og mønsterstrikk sett fra mange vinkler. Vi kjører også småkurs innenfor flere forskjellige felt og har aktiviterer for barn og ungdom begge dager. Vi fyller lokalet med små arbeidsstasjoner i diverse teknikker. Vær oppmerksom på at mye av påmeldinger til småkurs med mer skjer via hoopla.no. Påmeldingslenker kommer snart.</p>
+      <p>I år har vi et rikholdig program med alt fra foredrag innen Norsk ull, lokal produksjon og mønsterstrikk sett fra mange vinkler. Vi kjører også småkurs innenfor flere forskjellige felt og har aktiviterer for barn og ungdom begge dager. Vi fyller lokalet med små arbeidsstasjoner i diverse teknikker. Vær oppmerksom på at mye av påmeldinger til småkurs med mer skjer via hoopla.no! Påmeldingslenker kommer snart.</p>
       {programPosts
         .filter(post => post.node.frontmatter.title.length > 0)
         .map(({ node: post }) => {
-          const image = post.frontmatter.image != null ? post.frontmatter.image.childImageSharp.resize.src : require('./program/placeholder.jpg');          
+          const image = post.frontmatter.image != null ? post.frontmatter.image.childImageSharp.resize.src : require('./program/placeholder.jpg');
           return (
             <div className="event-preview" key={post.id}>
               <Link to={post.frontmatter.path}>
                 <div className="event-preview-main">
                   <div className="event-preview-left">
-                    <img src={image} alt='event image' className="event-image"/>
+                    <img src={image} alt='event image' className="event-preview-image" />
                   </div>
                   <div className="event-preview-right">
                     <div className='event-preview-header'>
                       <h3>{post.frontmatter.title}</h3>
-                      <h4>{post.frontmatter.day}</h4>
-                      <h4>{post.frontmatter.time}</h4>
-                      <h4>{post.frontmatter.location}</h4>
+                      <div className="event-preview-info">
+                        <h4>{post.frontmatter.day}</h4>
+                        <h4>-</h4>
+                        <h4>{post.frontmatter.time}</h4>
+                        <h4>-</h4>
+                        <h4>{post.frontmatter.location}</h4>
+                      </div>
                     </div>
                     <div className='event-preview-body'>
                       <p>{post.excerpt}</p>
--- src/templates/event.js
+++ src/templates/event.js
@@ -1,22 +1,39 @@
 import React from "react";
 import Helmet from "react-helmet";
+import '../styles/program.css';
 
 export default function Template({
   data
 }) {
   const post = data.markdownRemark;
-
+  const image = post.frontmatter.image != null ? post.frontmatter.image.childImageSharp.resize.src : require('./../pages/program/placeholder.jpg');
   return (
     <div className="event-container">
       <Helmet title={`Tromsø strikkefestival - ${post.frontmatter.title}`} />
-      <div className="event">
-        <h1>{post.frontmatter.title}</h1>
-        <h4>{post.frontmatter.date}</h4>
-        <div
-          className="event-content"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
+      <h1>{post.frontmatter.title}</h1>
+      <div className="event-info">
+        <div className="event-info-when">
+          <p className="event-info-title">
+            Når:
+          </p>
+          <p className="event-info-content">
+            {post.frontmatter.day} - {post.frontmatter.time}
+          </p>
+        </div>
+        <div className="event-info-where">
+          <p className="event-info-title">
+            Hvor:
+          </p>
+          <p className="event-info-content">
+            {post.frontmatter.location}
+          </p>
+        </div>
       </div>
+      <div
+        className="event-content"
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
+      <img src={image} alt='event image' className="event-image" />
     </div>
   );
 };
